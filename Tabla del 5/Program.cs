21407ff2f6e3222ce9131d3abb5551fad52f700c
Javier Lopez <javierlopez1001@gmail.com>
2021-05-21 18:06:09 -0500

    Se agrega el proyecto

---
+++ Tabla del 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabla_del_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+            Console.WriteLine("La tabla del 5");
+        
+            for(int i = 1; i <= 12; i++)
+            {
+                int multiplicar = num * i;
+                Console.WriteLine("{0} X {1} = {2}", num, i, multiplicar);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
